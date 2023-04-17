6abfd18fe5fa7a3f6567960f14092cde9424df38
ViktorLaishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-04-17 20:16:37 +0200

    Add files via upload

---
+++ solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result;
+        int maxCandies = *max_element(candies.begin(), candies.end());
+        for(int i = 0; i < candies.size(); i++){
+            if(candies[i] + extraCandies >= maxCandies){
+                result.push_back(true);
+            }else{
+                result.push_back(false);
+            }
+        }
+        return result;
+    }
+};
+
